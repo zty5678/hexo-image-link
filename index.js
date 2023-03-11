6ca66126d30cc458890ac9a5bfc20d10ad6903b5
zty5678 <zty5678@gmail.com>
2023-03-12 03:21:04 +0800

    add support for <img /> element

--- index.js
+++ index.js
@@ -2,7 +2,27 @@
 
 // match markdown image and covert to asset_img 
 hexo.extend.filter.register('before_post_render', function(data){
-
+    /**
+     * 如果遇到了img标签, 做如下转换：
+     * <img src="./xxxxx/image1.jpg" alt="image1" style="zoom:50%;" />
+     * -> 
+     * <img src="../xxxxx/image1.jpg" alt="image1" style="zoom:50%;" />  
+     * 目的是同时支持hexo和Typora编辑器
+     */
+    data.content = data.content.replace(/<img.*?src="(.*?)"/g,
+        function(match_str, srcAttr){ 
+            
+            if (srcAttr.startsWith("./")) {
+                var oldSrc= srcAttr;
+                var newSrc = "."+srcAttr;
+                return match_str.replace(oldSrc, newSrc);
+            }
+            return match_str;
+        
+        }); 
+    /**
+     * 将![label](path1.jpg)的markdown语法转换成 {% asset_img path1.jpg label %} 的语法
+     */
     data.content = data.content.replace(/!{1}\[([^\[\]]*)\]\((.*)\s?(?:".*")?\)/g,
         function(match_str, label, path){
 
